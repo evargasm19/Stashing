76ee4321bbe30a66041dee4f18c31afecef752fd
evargasm19 <evargasm19@gmail.com>
2025-02-24 03:32:14 -0500

    uploading the java project

---
+++ src/stash/Template.java
@@ -0,0 +1,16 @@
+package stash;
+/**
+ * to test stashing
+ */
+class  TemplateImplemetation {
+    String display(){
+        String s = "Find solutions";
+        return s;
+    }
+}
+public class Template {
+    public static void main(String args[]) {
+        TemplateImplemetation objvar = new TemplateImplemetation();
+        System.out.println(objvar.display() );
+    }
+}
